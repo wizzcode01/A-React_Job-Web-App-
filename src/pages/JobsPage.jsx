82822fcea760feb7ebf8c5e8c0ce504d2239f3b4
wizzycode <dicksonwisdom005@gmail.com>
2025-06-21 22:12:27 +0100

    first commit

---
+++ src/pages/JobsPage.jsx
@@ -0,0 +1,12 @@
+import JobListing from "../components/JobListing"
+import JobListings from "../components/JobListings" 
+
+const JobsPage = () => {
+    return (
+        <section className="bg-blue-50 px-4 py-6">
+           <JobListings/> 
+        </section>
+    )
+}
+
+export  default JobsPage
