52596d583bdc5e678be6bc1e812f8e13450edbd1
wizzycode <dicksonwisdom005@gmail.com>
2025-07-18 06:27:28 +0100

    fix

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: process.env.VITE_BASE_PATH || "/react-vite-jobapp",
   server: {
     port: 3000,
     proxy: {
